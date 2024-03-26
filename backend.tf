5cfc9dfd08057a78492cf240dec03d0073fdbbe1
Heena122 <126157263+Heena122@users.noreply.github.com>
2024-03-26 14:22:07 +0530

    updatee

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "my-terraform-state-bucket"
-#     key            = "terraform.tfstate"
-#     region         = "us-east-1"
-#     dynamodb_table = "terraform-state-lock"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "heena123987"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+  }
+}
