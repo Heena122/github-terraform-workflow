fbb74e6b4574df3f6469b26bdfd38f089e72bfe7
Heena <wadhwaheena28@gmail.com>
2024-05-30 18:16:32 +0530

    new commit11113

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "s3" {
-    bucket         = "heena123989"
-    key            = "terraform.tfstate"
-    region         = "eu-north-1"
-    dynamodb_table = "table-store"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "heena123989"
+#     key            = "terraform.tfstate"
+#     region         = "eu-north-1"
+#     dynamodb_table = "table-store"
+#   }
+# }
