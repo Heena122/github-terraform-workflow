50ffa7d3baedf370a83c75b16fbb99af9eb78797
Heena <wadhwaheena28@gmail.com>
2024-06-04 13:07:23 +0530

    dskajgfhas

--- backend.tf
+++ backend.tf
@@ -4,8 +4,8 @@ terraform {
     key            = "terraform.tfstate"
     region         = "eu-north-1"
     dynamodb_table = "table-store"
-    access_key = "AKIA4MTWLFU2ZDMBTD4S"
-  secret_key = "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"
+   
 
   }
 }
+
--- provider.tf
+++ provider.tf
@@ -1,7 +1,6 @@
 provider "aws" {
     region = "eu-north-1"
-     access_key = "AKIA4MTWLFU2ZDMBTD4S"
-  secret_key = "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"
+  
   
 }
 terraform {
